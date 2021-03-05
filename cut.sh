7fefce49a630638178fd1fe5cdd2d070ef9a9ffd
DarthVanger <darthvanger@gmail.com>
2021-03-05 17:05:24 +0200

    Correct a comment

--- cut.sh
+++ cut.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 ########
-# Cut video ./video.mp4 into multiple pieces.
-# Each line creates a piece in ./output/pieces/{piece-name}.mp4.
-
-# Piece start is the time passed as -ss arg.
-# Piece duration is in seconds, passed as -t arg.
-#
-# To add a new piece, copy a line and change name of the output file.
+# Cut videos into pieces.
+# -ss is start time of cut
+# -t is duration of output
 ########
 
 output_folder="output/pieces"
